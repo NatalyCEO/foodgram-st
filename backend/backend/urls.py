ff372960a07875489e41a1794e729df53c26064a
NatalyCEO <miss.razzhivina@list.ru>
2025-05-31 23:58:29 +0300

    Update 4

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,3 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-"""
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
